e7c229c207fed4398ac1d08504bc4e46f1a6172b
Kushagra <143399362+KushagraSharma924@users.noreply.github.com>
2024-08-04 11:36:12 +0530

    frontend fetch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { Createtodo } from './components/CreateTodo'
+import { CreateTodo } from './components/CreateTodo'
+import { Todos } from './components/Todos'
 
+// useEffect hook
 function App() {
-  return(
+  const [todos, setTodos] = useState([]);
+
+  // fetch("http://localhost:3000/todos")
+  //   .then(async function(res) {
+  //     const json = await res.json();
+  //     setTodos(json.todos);
+  //   })
+
+  return (
     <div>
-       <Createtodo/>
+      <CreateTodo></CreateTodo>
+      <Todos todos={todos}></Todos>
     </div>
   )
 }
